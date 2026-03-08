c399a11e6f0de2d12e4e5388dea5d4981fce3226
mishaal <26100247@lums.edu.pk>
2026-03-08 22:07:16 +0500

    cd changes made and  sprint 4 code fixed

---
+++ P08-Agri/Development/Sprint-4/code/frontend/src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+jest.mock('axios', () => ({
+  __esModule: true,
+  default: {
+    create: () => ({
+      post: jest.fn(),
+      get: jest.fn()
+    })
+  }
+}))
+
+test('renders login screen by default', () => {
+  render(<App />)
+  expect(screen.getByText(/welcome back/i)).toBeInTheDocument()
+})
